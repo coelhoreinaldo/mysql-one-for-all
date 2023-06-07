6b0286f31027eb1bc38d3ad5f7099fdd76a8f8a4
coelhoreinaldo <scoelhor7@gmail.com>
2023-06-06 21:05:39 -0300

    ♻ refactor: group by alias, req 4

--- challenges/desafio4.sql
+++ challenges/desafio4.sql
@@ -1,4 +1,4 @@
 SELECT p.nome AS pessoa_usuaria, IF(MAX(YEAR(h.data_reproducao)) < 2021, 'Inativa', 'Ativa') AS status_pessoa_usuaria 
 FROM SpotifyClone.pessoa_usuaria AS p
 INNER JOIN SpotifyClone.historico_de_reproducoes AS h ON h.pessoa_usuaria_id = p.pessoa_usuaria_id
-GROUP BY p.nome ORDER BY p.nome;
+GROUP BY pessoa_usuaria ORDER BY pessoa_usuaria;

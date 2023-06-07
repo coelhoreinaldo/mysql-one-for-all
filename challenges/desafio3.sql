e90169d2e4b3b425ff23a659b880e65474ff173b
coelhoreinaldo <scoelhor7@gmail.com>
2023-06-07 14:09:52 -0300

    🔧 fix: req 3

--- challenges/desafio3.sql
+++ challenges/desafio3.sql
@@ -2,5 +2,5 @@ SELECT p.nome AS pessoa_usuaria, COUNT(h.cancao_id) AS musicas_ouvidas, ROUND(SU
 FROM SpotifyClone.pessoa_usuaria AS p
 INNER JOIN SpotifyClone.historico_de_reproducoes AS h ON p.pessoa_usuaria_id = h.pessoa_usuaria_id
 INNER JOIN SpotifyClone.cancao AS c ON h.cancao_id = c.cancao_id
-GROUP BY p.nome 
-ORDER BY p.nome; 
+GROUP BY pessoa_usuaria 
+ORDER BY pessoa_usuaria;

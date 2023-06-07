4b73d97196f52db89571676fa9889828177fc5aa
coelhoreinaldo <scoelhor7@gmail.com>
2023-06-07 10:55:53 -0300

    ♻ refactor: req 10, correct data

--- challenges/desafio10.sql
+++ challenges/desafio10.sql
@@ -8,16 +8,16 @@ CREATE TABLE cancao_favorita
 );
 
 INSERT INTO cancao_favorita (pessoa_usuaria_id, cancao_id) VALUES
-    (1, 1),
-    (1, 2),
     (1, 3),
+    (1, 6),
+    (1, 10),
     (2, 4),
-    (3, 5),
     (3, 1),
-    (4, 6),
+    (3, 3),
+    (4, 7),
     (4, 4),
-    (5, 3),
-    (5, 7),
-    (6, 4),
-    (7, 6),
-    (8, 1);
+    (5, 10),
+    (5, 2),
+    (8, 4),
+    (9, 7),
+    (10, 3);
